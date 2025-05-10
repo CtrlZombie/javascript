ca4521ec2d6b00a5e26e4bbf8d22f4637009a5d6
CtrlZombie <ivan.kletnov95@gmail.com>
2025-05-10 22:41:30 +0500

    задание к уроку Компоненты языка JavaScript

---
+++ script.js
@@ -0,0 +1,9 @@
+const city = 'Magnitogorsk' //Присвоение переменной city значения Magnitogorsk
+
+const temperature = '14°' //Присвоение переменной temperature значения 14°
+
+console.log(city, temperature) //Вывод информации в консоль
+
+const result = (17+10) * 4 //Присвоение выражения переменной result
+
+console.log(result) //вывод результата выражения в консоль
